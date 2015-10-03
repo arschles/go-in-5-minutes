eb188a65d75fbfa98d2df70ae163a6dd89e77390
Aaron Schlesinger <arschles@gmail.com>
2015-10-03 11:26:16 -0700

    more docs

--- episode1/main.go
+++ episode1/main.go
@@ -18,10 +18,13 @@ func main() {
 	router.Handle("/{key}", handlers.GetKey(db)).Methods("GET") // get the value of a key
 	router.Handle("/{key}", handlers.PutKey(db)).Methods("PUT") // set the value of a key
 
-	// http.ListenAndServe takes in an http.Handler as its second parameter.
-	// since router is an http.ServeMux, and http.ServeMux is also a http.Handler
-	// implementation (because it has a ServeHTTP func - https://godoc.org/net/http#ServeMux.ServeHTTP),
-	// we can pass it here
 	log.Printf("serving on port 8080")
-	http.ListenAndServe(":8080", router)
+
+	// http.ListenAndServe takes in an http.Handler as its second parameter.
+	// since router is a Gorilla router (https://godoc.org/github.com/gorilla/mux#Router)
+	// which is an http.Handler implementation, we can pass it here. Note that we could
+	// also pass an http.ServeMux if we wanted to because it implements http.Handler
+	// by providing a ServeHTTP func (https://godoc.org/net/http#ServeMux.ServeHTTP)
+	err := http.ListenAndServe(":8080", router)
+	log.Fatal(err)
 }
