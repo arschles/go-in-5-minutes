437fd48ab8b6fa00ed303e419050a2aee3a424d9
Aaron Schlesinger <arschles@gmail.com>
2015-10-15 14:10:16 -0700

    adding matcher func

--- episode3/gorilla_server.go
+++ episode3/gorilla_server.go
@@ -30,17 +30,32 @@ var servers = map[string]*ServerStatus{
 var mx sync.RWMutex
 
 func main() {
+	// creates a new top level mux.Router. since a mux.Router implements the http.Handler interface,
+	// we can pass it to http.ListenAndServe below
 	router := mux.NewRouter()
+
+	// configure the router to always run this handler when it couldn't match a request to any other handler
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(fmt.Sprintf("%s not found\n", r.URL)))
 	})
 
-	router.HandleFunc("/api/{name}", getServer).Methods("GET")
-	router.HandleFunc("/api/{name}", reserveServer).Methods("POST")
-	router.HandleFunc("/api/{name}", releaseServer).Methods("DELETE")
+	// create a subrouter just for standard API calls. subrouters are convenient ways to
+	// group similar functionality together.
+	apiRouter := router.Headers("Content-Type", "application/json").Subrouter()
+	apiRouter.HandleFunc("/api/{name}", getServer).Methods("GET")
+	apiRouter.HandleFunc("/api/{name}", reserveServer).Methods("POST")
+	apiRouter.HandleFunc("/api/{name}", releaseServer).Methods("DELETE")
 
-	adminAPIRouter := router.Headers("X-ADMIN-TOKEN", "SuperAdmin").Subrouter()
+	adminAPIRouter := router.Headers("Content-Type", "application/json").MatcherFunc(func(r *http.Request, rm *mux.RouteMatch) bool {
+		adminToken := r.Header.Get("X-ADMIN-TOKEN")
+		if adminToken == "" {
+			return false
+		}
+		// in a production setting, this could be a DB lookup to determine whether the token
+		// is valid for admin usage
+		return adminToken == "SuperAdmin"
+	}).Subrouter()
 	adminAPIRouter.HandleFunc("/api/admin/servers", getAllServers).Methods("GET")
 	adminAPIRouter.HandleFunc("/api/admin/servers", releaseAllServers).Methods("DELETE")
 
