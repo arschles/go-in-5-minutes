f7510640c481dcc4c45fc7f6863b328c2ea00236
Aaron Schlesinger <arschles@gmail.com>
2015-10-31 20:26:44 -0700

    inmem db impl

--- episode5/models/db.go
+++ episode5/models/db.go
@@ -1,10 +1,23 @@
 package models
 
+import "errors"
+
+var ErrNotFound = errors.New("not found")
+
 // DB is the interface to the database. The idea here is to have multiple
 // different DB implementations. For example, you could build an in-memory
 // one for testing and a MongoDB one for production
 type DB interface {
+	// Get gets the value for the given key. returns the value and nil
+	// if it was found, nil and ErrNotFound if it was not found and another
+	// appropriate error otherwise
 	Get(key string) ([]byte, error)
+	// Set sets the value for the given existing key. returns nil if the key
+	// already existed and was successfully set, ErrNotFound if the key didn't
+	// already exist, and another appropriate error otherwise
 	Set(key string, val []byte) error
+	// Upsert sets or creates the value for the existing key. returns true and nil
+	// if the key was created on this call, false and nil if the key was not created
+	// but still successfully updated, and false and the appropriate error otherwise
 	Upsert(key string, val []byte) (bool, error)
 }
--- episode5/models/in_memory_db.go
+++ episode5/models/in_memory_db.go
@@ -1,5 +1,44 @@
 package models
 
-func NewInMemoryDB() DB {
+import "sync"
 
+type inMemDB struct {
+	rwm *sync.RWMutex
+	m   map[string][]byte
+}
+
+func NewInMemoryDB() DB {
+	return &inMemDB{
+		rwm: &sync.RWMutex{},
+		m:   make(map[string][]byte),
+	}
+}
+
+func (db *inMemDB) Get(key string) ([]byte, error) {
+	db.rwm.RLock()
+	defer db.rwm.RUnlock()
+	val, ok := db.m[key]
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return val, nil
+}
+
+func (db *inMemDB) Set(key string, val []byte) error {
+	db.rwm.Lock()
+	defer db.rwm.Unlock()
+	_, ok := db.m[key]
+	if !ok {
+		return ErrNotFound
+	}
+	db.m[key] = val
+	return nil
+}
+
+func (db *inMemDB) Upsert(key string, val []byte) (bool, error) {
+	db.rwm.Lock()
+	defer db.rwm.Unlock()
+	_, ok := db.m[key]
+	db.m[key] = val
+	return !ok, nil
 }
