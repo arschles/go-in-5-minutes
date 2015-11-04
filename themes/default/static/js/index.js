7f26f2dac343361296e5dbf5e4d1220661588bb6
Aaron Schlesinger <arschles@gmail.com>
2015-11-03 19:34:55 -0800

    landing page

--- themes/default/static/js/index.js
+++ themes/default/static/js/index.js
@@ -4,10 +4,8 @@ $("#rotating-topics").Morphext({
     // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
     separator: ",",
     // The delay between the changing of each phrase in milliseconds.
-    speed: 3000,
+    speed: 2000,
     complete: function () {
         // Called after the entrance animation is executed.
     }
 });
-
-// $("#gifm-row-1").height($(document).height());
