e0dd79457efa68d07903890771d18a987d360491
Aaron Schlesinger <aaronschlesinger@ENG000656.local>
2015-11-16 20:22:24 -0700

    more docs, etc...

--- episode7/glideify.sh
+++ episode7/glideify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# this script won't work if you've already run it once.
-# it creates a glide.yaml file to track dependencies and a
-# vendor/ folder to house the code to build each dependency
+
+# Attention: remove the vendor/ directory and the glide.yaml file before running this script. It will fail to run if you run it against this repository as-is, or you've already successfully run it once.
+
+# Details: This script creates a glide.yaml file to track dependencies, and a vendor/ folder to store them. Neither operation is idempotent, so they must be manually removed.
+
 glide create # create the glide.yaml file
 
 # install first level dependencies
@@ -13,6 +15,6 @@ glide get github.com/arschles/testsrv # download the latest testsrc code into ve
 glide get github.com/google/go-querystring/query
 
 
-# optional commands for later
+# Special note: The following are optional commands for project maintenance
 glide install # run this command to install updates for all dependencies listed in glide.yaml
 glide up github.com/gorilla/mux # run this command to only install updates for gorilla mux
