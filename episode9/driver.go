a817959648dcefe7e3891eb5c7dcb986e6693aa2
Aaron Schlesinger <arschles@gmail.com>
2015-12-06 17:26:22 -0800

    adding some comments

--- episode9/driver.go
+++ episode9/driver.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
+	// create the dedicated implementation
 	mem := conf.NewMem()
+	// use the default implementation (the singleton)
 	conf.SetInt("a", 1)
+	// use the dedicated implementation
 	mem.SetInt("a", 2)
+
+	// the remaining lines prove that the dedicated implementation is separate from the default (singleton) implementation
 	i, err := conf.GetInt("a")
 	if err != nil {
 		log.Printf("(default) error getting int 'a' [%s]", err)
