11af19006a0ddd2700ccc451c7be5fd7ffcf5d1a
Aaron Schlesinger <arschles@gmail.com>
2015-12-13 17:41:52 -0800

    run hugo before deploy

--- deploy-site.sh
+++ deploy-site.sh
@@ -2,6 +2,14 @@
 
 JWT_FILE="gifm-jwt.json"
 touch $JWT_FILE
-echo $GCSUP_JWT_FILE > $JWT_FILE
+JWT=echo "$GCSUP_JWT" | base64 -D
+echo "$JWT" > $JWT_FILE
 
-docker run --rm -v $PWD:/pwd -w /pwd -e GCSUP_JWT_FILE_LOCATION=$JWT_FILE -e GCSUP_PROJECT_NAME=$GCS_PROJECT_NAME -e quay.io/arschles/gcsup:0.0.1 gcsup
+# build in hugo
+docker run --rm -v "$PWD/www":/www -w /www quay.io/arschles/hugo:latest hugo -v
+
+docker run --rm -v $PWD:/pwd -w /pwd \
+  -e GCSUP_JWT_FILE_LOCATION=$JWT_FILE \
+  -e GCSUP_PROJECT_NAME=$GCS_PROJECT_NAME \
+  -e GCSUP_BUCKET_NAME=$GCSUP_BUCKET_NAME \
+  -e GCSUP_LOCAL_FOLDER=www/public quay.io/arschles/gcsup:0.0.1 gcsup
