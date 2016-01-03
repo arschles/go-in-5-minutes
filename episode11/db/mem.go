e66af71c4a55ce9fe1dac398ef2ca3ece4768ef8
Aaron Schlesinger <arschles@gmail.com>
2016-01-03 12:19:29 -0800

    fixing lookup
    by string, not key which will change across invocations

--- episode11/db/mem.go
+++ episode11/db/mem.go
@@ -11,31 +11,31 @@ var ErrNotFound = errors.New("not found")
 
 type Mem struct {
 	mut sync.RWMutex
-	m   map[models.Key]models.Model
+	m   map[string]models.Model
 }
 
 func NewMem() *Mem {
-	return &Mem{m: make(map[models.Key]models.Model)}
+	return &Mem{m: make(map[string]models.Model)}
 }
 
 func (m *Mem) Save(key models.Key, model models.Model) error {
 	m.mut.Lock()
 	defer m.mut.Unlock()
-	m.m[key] = model
+	m.m[key.String()] = model
 	return nil
 }
 
 func (m *Mem) Delete(key models.Key) error {
 	m.mut.Lock()
 	defer m.mut.Unlock()
-	delete(m.m, key)
+	delete(m.m, key.String())
 	return nil
 }
 
 func (m *Mem) Get(key models.Key, model models.Model) error {
 	m.mut.RLock()
 	defer m.mut.RUnlock()
-	md, ok := m.m[key]
+	md, ok := m.m[key.String()]
 	if !ok {
 		return ErrNotFound
 	}
