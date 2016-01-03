3c55199ea04d7a96afb54caa03615b2867008577
Aaron Schlesinger <arschles@gmail.com>
2016-01-03 13:53:35 -0800

    add link to gcsup issue

--- deploy-site.sh
+++ deploy-site.sh
@@ -5,7 +5,8 @@ touch $JWT_FILE
 JWT=$(echo "$GCSUP_JWT" | base64 -D)
 echo "$JWT" > $JWT_FILE
 
-# note the need for GOOGLE_APPLICATION_CREDENTIALS. still working on that...
+# note the need for GOOGLE_APPLICATION_CREDENTIALS. it's needed by gcsup.
+# see https://github.com/arschles/gcsup/issues/4
 docker run --rm -v $PWD:/pwd -w /pwd \
   -e GCSUP_JWT_FILE_LOCATION=/pwd/$JWT_FILE \
   -e GCSUP_PROJECT_NAME=go-in-5-minutes \
