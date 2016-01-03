bfc98fe3fc293ee63a64e9c0ec719540a81c99e0
Aaron Schlesinger <arschles@gmail.com>
2016-01-03 12:17:13 -0800

    implementing get

--- episode11/handlers/get_app.go
+++ episode11/handlers/get_app.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/arschles/go-in-5-minutes/episode11/db"
+	"github.com/arschles/go-in-5-minutes/episode11/models"
 	"github.com/gorilla/mux"
 )
 
@@ -21,5 +23,18 @@ func (c *GetAppHandler) RegisterRoute(r *mux.Router) {
 }
 
 func (c *GetAppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	name, ok := mux.Vars(r)[appNamePath]
+	if !ok {
+		http.Error(w, jsonErrStr("missing app name in path"), http.StatusBadRequest)
+		return
+	}
+	app := &models.App{}
+	if err := c.db.Get(models.NewAppKey(name), app); err != nil {
+		http.Error(w, jsonErrStr(fmt.Sprintf("database error: %s", err)), http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(app); err != nil {
+		http.Error(w, jsonErr(err, "encoding json"), http.StatusInternalServerError)
+		return
+	}
 }
