5bb216e1b9cd6d5541f53ec46286d8a3a2e2c73f
Aaron Schlesinger <arschles@gmail.com>
2016-05-15 14:35:23 -0700

    adding create table, insert and select

--- episode13/main.go
+++ episode13/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
-	_ "github.com/mxk/go-sqlite/sqlite3"
 	"log"
+
+	"github.com/arschles/go-in-5-minutes/episode13/models"
+	_ "github.com/mxk/go-sqlite/sqlite3"
 )
 
 const (
@@ -16,5 +18,25 @@ func main() {
 	if err != nil {
 		log.Fatalf("error opening DB (%s)", err)
 	}
+
+	log.Printf("Creating new table")
+	if _, crErr := models.CreatePersonTable(db); crErr != nil {
+		log.Fatalf("Error creating table (%s)", crErr)
+	}
+	log.Printf("Created")
+
+	person := models.Person{FirstName: "Aaron", LastName: "Schlesinger", Age: 29}
+	log.Printf("Inserting %+v into the DB", person)
+	if _, insErr := models.InsertPerson(db, person); insErr != nil {
+		log.Fatalf("Error inserting new person into the DB (%s)", insErr)
+	}
+	log.Printf("Inserted")
+
+	log.Printf("Selecting person from the DB")
+	retPerson := models.Person{}
+	if err := models.SelectPerson(db, person.FirstName, person.LastName, person.Age, &retPerson); err != nil {
+		log.Fatalf("Error selecting person from the DB (%s)", err)
+	}
+	log.Printf("Selected %+v from the DB", retPerson)
 
 }
--- episode13/models/person.go
+++ episode13/models/person.go
@@ -2,13 +2,18 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 const (
-	PersonTableName    = "person"
+	// PersonTableName is the name of the table for the person model
+	PersonTableName = "person"
+	// PersonFirstNameCol is the column name of the model's first name
 	PersonFirstNameCol = "first_name"
-	PersonLastNameCol  = "last_name"
-	PersonAgeCol       = "age"
+	// PersonLastNameCol is the column name of the model's last name
+	PersonLastNameCol = "last_name"
+	// PersonAgeCol is the column name of the model's age
+	PersonAgeCol = "age"
 )
 
 // Person is the database model for a person
@@ -21,10 +26,46 @@ type Person struct {
 // CreatePersonTable uses db to create a new table for Person models, and returns the result
 func CreatePersonTable(db *sql.DB) (sql.Result, error) {
 	return db.Exec(
-		"CREATE TABLE ? (? varchar(255), ? varchar(255), ? int)",
-		PersonTableName,
-		PersonFirstNameCol,
-		PersonLastNameCol,
-		PersonAgeCol,
+		fmt.Sprintf("CREATE TABLE %s (%s varchar(255), %s varchar(255), %s int)",
+			PersonTableName,
+			PersonFirstNameCol,
+			PersonLastNameCol,
+			PersonAgeCol,
+		),
 	)
+}
+
+// InsertPerson inserts person into db
+func InsertPerson(db *sql.DB, person Person) (sql.Result, error) {
+	return db.Exec(
+		fmt.Sprintf("INSERT INTO %s VALUES(?, ?, ?)", PersonTableName),
+		person.FirstName,
+		person.LastName,
+		person.Age,
+	)
+}
+
+// SelectPerson selects a person with the given first & last names and age. On success, writes the result into result and on failure, returns a non-nil error and makes no modifications to result
+func SelectPerson(db *sql.DB, firstName, lastName string, age uint, result *Person) error {
+	row := db.QueryRow(
+		fmt.Sprintf(
+			"SELECT * FROM %s WHERE %s=? AND %s=? AND %s=?",
+			PersonTableName,
+			PersonFirstNameCol,
+			PersonLastNameCol,
+			PersonAgeCol,
+		),
+		firstName,
+		lastName,
+		age,
+	)
+	var retFirstName, retLastName string
+	var retAge uint
+	if err := row.Scan(&retFirstName, &retLastName, &retAge); err != nil {
+		return err
+	}
+	result.FirstName = retFirstName
+	result.LastName = retLastName
+	result.Age = retAge
+	return nil
 }
