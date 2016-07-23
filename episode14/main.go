bd10dc1e1e1b74f9947420752a65fff0969934dd
Aaron Schlesinger <arschles@gmail.com>
2016-07-23 13:31:39 -0700

    complete template & query string aggregation

--- episode14/main.go
+++ episode14/main.go
@@ -8,14 +8,17 @@ import (
 	"os"
 )
 
+// hdl is a handler that calls t.Execute, passing all of the query string values as data to the template
 func hdl(t *template.Template) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tplData := map[string]string{}
 		// iterate through all query string values. overwrite keys that occur twice in the query string
-		for keys, vals := range r.URL.Query() {
-			for 
-			tplData[k] = v
+		for key, vals := range r.URL.Query() {
+			for _, val := range vals {
+				tplData[key] = val
+			}
 		}
+		// Notice that t.Execute takes an io.Writer as its first argument. The variable w (an http.ResponseWriter) implements io.Writer, so we pass it and allow the implementation to call its Write function as necessary
 		if err := t.Execute(w, tplData); err != nil {
 			http.Error(w, fmt.Sprintf("error executing template (%s)", err), http.StatusInternalServerError)
 		}
