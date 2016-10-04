4f258ac8ede6b096323aa9ce6bc394b2a6795619
Aaron Schlesinger <arschles@gmail.com>
2016-10-03 17:13:24 -0700

    replacing linked list with set tests

--- episode16/set.go
+++ episode16/set.go
@@ -1,7 +1,7 @@
 package main
 
 type setElement struct {
-	value string
+	val string
 }
 
 type set struct {
@@ -17,6 +17,23 @@ func (s *set) exists(elt setElement) bool {
 	return ok
 }
 
-func (s *set) add(elt setElement) {
+func (s *set) add(elt setElement) bool {
+	_, ok := s.m[elt]
 	s.m[elt] = struct{}{}
+	return !ok
+}
+
+func (s *set) remove(elt setElement) bool {
+	_, ok := s.m[elt]
+	if ok {
+		return false
+	}
+	delete(s.m, elt)
+	return true
+}
+
+func (s *set) removeAll() int {
+	ret := len(s.m)
+	s.m = make(map[setElement]struct{})
+	return ret
 }
