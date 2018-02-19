0d2159c9dac6fee64a07a81af10bb97d3b771557
Aaron Schlesinger <arschles@gmail.com>
2018-02-19 14:55:24 -0800

    adding more comments

--- episode20/all_test.go
+++ episode20/all_test.go
@@ -13,15 +13,23 @@ type TransformerTests struct {
 }
 
 func (t *TransformerTests) SetupSuite() {
-	// logic in here will run once, before all tests run
-	t.transformer = &transformer{
-		mut:   new(sync.Mutex),
-		cache: map[string]interface{}{},
-	}
+	// runs once before the entire test suite runs. you could so something in this function
+	// like set up some global state. here are a few common tasks to consider:
+	//
+	// - initialize the global random number generator:
+	//		rand.Seed(time.Now().UnixNano())
+	// - create & seed a database
 }
 
 func (t *TransformerTests) SetupTest() {
-	// logic in here will run before every individual test runs
+	// runs once before every individual test runs. you can do things like clear common
+	// state so that everything is "clean" for the test to run. be careful if you're
+	// doing things to an external database, though. by default, tests run in parallel
+	// so if all your tests share the same database and tables, they'll be overwriting
+	// each other.
+	//
+	// you can fix that by either using unique DB/table names or running the tests without
+	// parallelism (go test -p 1 ./...)
 	t.transformer = &transformer{
 		mut:   new(sync.Mutex),
 		cache: map[string]interface{}{},
@@ -29,11 +37,13 @@ func (t *TransformerTests) SetupTest() {
 }
 
 func (t *TransformerTests) TearDownTest() {
-	// logic in here will run after every individual test runs
+	// runs once after every individual test runs. you can clean up after your tests here.
+	// a common thing here is to shut down an HTTP server that you left running
 }
 
 func (t *TransformerTests) TearDownSuite() {
-	// logic in here will run once, after all tests run
+	// runs once after the entire test suite runs. a common thing to do here is shut down
+	// a database you created before the tests ran
 }
 
 func TestTransformer(t *testing.T) {
