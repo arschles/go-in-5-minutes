c15e749f1333f9827677046352a12bec1daf89bc
Aaron Schlesinger <arschles@gmail.com>
2018-05-24 12:07:14 -0700

    spacing on the home handler & test

--- episode22/actions/home.go
+++ episode22/actions/home.go
@@ -5,5 +5,5 @@ import "github.com/gobuffalo/buffalo"
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-  return c.Render(200, r.HTML("index.html"))
-  }
+	return c.Render(200, r.HTML("index.html"))
+}
