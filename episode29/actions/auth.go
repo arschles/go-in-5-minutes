74d79554d3ee2b183323d239d370f6f15173f99a
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2019-08-13 10:56:31 -0700

    Episode 29 - Login with GitHub with Buffalo (#179)
    * Episode 29 - Login with GitHub with Buffalo
    
    * removing subrepo tracker
    
    * Adding real episode29 code
    
    * more stuff
    
    * adding the goth plugin
    
    * stuff!
    
    * Remove cruft
    
    * adding web page for episode 29

---
+++ episode29/actions/auth.go
@@ -0,0 +1,27 @@
+package actions
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+)
+
+func setupGoth(app *buffalo.App) {
+	// gothic.Store = app.SessionStore
+	goth.UseProviders(
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), fmt.Sprintf("http://127.0.0.1:3000/auth/github/callback")),
+	)
+}
+
+func AuthCallback(c buffalo.Context) error {
+	user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
+	if err != nil {
+		return c.Error(401, err)
+	}
+	// Do something with the user, maybe register them/sign them in
+	return c.Render(200, r.JSON(user))
+}
