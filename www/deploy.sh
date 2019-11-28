5ce295da6307ea0e7ec589ed6d9b4146a408f8bc
Aaron Schlesinger <aaron@ecomaz.net>
2019-11-28 15:30:02 -0800

    Adding deprecated notice to the old GCS deploy script

--- www/deploy.sh
+++ www/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This file is deprecated and unused. It was needed when the GIFM
+# site was hosted on Google Cloud Storage. It was since moved to 
+# Netlify (https://netlify.com) 
+
 gsutil rsync -R public gs://www.goin5minutes.com
 gsutil -m acl ch -R -u AllUsers:R gs://www.goin5minutes.com
 gsutil web set -m index.html -e 404.html gs://www.goin5minutes.com
